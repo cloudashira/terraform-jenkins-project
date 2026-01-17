9e7fe1153d655d06b8c7338c2e3ad872d2e8c6d3
ashira <cloudashira@gmail.com>
2026-01-17 07:45:00 +0000

    use default VPC and subnet

--- main.tf
+++ main.tf
@@ -2,18 +2,23 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Get all subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_instance" "demo" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  subnet_id     = data.aws_subnets.default.ids[0]   # pick the first subnet
 
   tags = {
     Name = "Jenkins-Terraform-EC2"
