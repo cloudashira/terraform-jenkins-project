4f76859ab7bc831a045744941e215580d83db66e
ashira <cloudashira@gmail.com>
2026-01-18 05:09:54 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-provider "aws" {
+provider "azure" {
   region = "ap-south-1"
 }
 
 # Get your custom VPC by ID
-data "aws_vpc" "jenkins" {
+data "azure_vpc" "jenkins" {
   id = "vpc-03101880556ae0a7a"
 }
 
 # Get all subnets inside this VPC
-data "aws_subnets" "jenkins" {
+data "azure_subnets" "jenkins" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.jenkins.id]
+    values = [data.azure_vpc.jenkins.id]
   }
 }
 
 # EC2 instance
-resource "aws_instance" "demo" {
+resource "azure_instance" "demo" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
   subnet_id     = "subnet-02bed3b0353321c9f"  # <- quotes added
